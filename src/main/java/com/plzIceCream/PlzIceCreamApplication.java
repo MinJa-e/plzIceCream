193f2b4c79ca3d61caf9fe2910cdf6a0ae3834d8
codeInJ <80948609+codeInJ@users.noreply.github.com>
2022-02-07 21:05:20 +0900

    plzIceCream 첫 커밋 테스트

---
+++ src/main/java/com/plzIceCream/PlzIceCreamApplication.java
@@ -0,0 +1,14 @@
+package com.plzIceCream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class PlzIceCreamApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlzIceCreamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/plzIceCream/PlzIceCreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plzIceCream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlzIceCreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
